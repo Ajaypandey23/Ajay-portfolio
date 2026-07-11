633ad05f617467d543f1a9a5a1fa40478e052d45
Ajaypandey23 <pandeyajay2448@gmail.com>
2026-07-11 14:35:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   const revealEls = document.querySelectorAll('.reveal')
   const io = new IntersectionObserver((entries)=>{
     entries.forEach(e=>{
-      if(e.isIntersecting) e.target.classList.add('visible')
+      if(e.isIntersecting) e.target.classList.add('in')
     })
   },{threshold:0.08})
   revealEls.forEach(el=>io.observe(el))
